b2fb5ea9121846e26de7e64ce66f6cb7aecf39ea
Michael Alexander <alexandermw@gmail.com>
2014-08-14 11:42:07 -0700

    Create caesar_cipher_spec.rb

---
+++ caesar_cipher_spec.rb
@@ -0,0 +1,29 @@
+describe "Caesar" do
+  describe "#caesar_cipher" do
+    before {@c = Caesar.new}
+  
+    it "shifts characters in a string by the number provided" do
+      expect(@c.caesar_cipher("abc", 3)) to eql "def"
+    end
+  
+    it "converts both uppercase and lowercase characters correctly" do
+      expect(@c.caesar_cipher("AbCd", 3)) to eql "DeFg"
+    end
+    
+    it "leaves spaces and punctuation alone" do
+      expect(@c.caesar_cipher("A. b? Cd!!!!", 3)) to eql "D. e? Fg!!!!"
+    end
+    
+    it "correctly wraps around shifts greater than 26"
+      expect(@c.caesar_cipher("abc", 29)) to eql @c.caesar_cipher("abc", 3)
+    end
+    
+    it "takes negative shift values" do
+      expect (@c.caesar_cipher("abc", -3)) to eql "xyz"
+    end
+
+    it "can decode itself"
+      expect (@c.caesar_cipher((@c.caesar_cipher("bleh blah Bluh!", 18)), -18)) to eql "bleh blah Bluh!"
+    end
+  end
+end
