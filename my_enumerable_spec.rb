bb524dab2169c0228203da4bd7b50270503dc8b5
Michael Alexander <alexandermw@gmail.com>
2014-08-17 16:35:43 -0700

    wrote #my_count tests

--- my_enumerable_spec.rb
+++ my_enumerable_spec.rb
@@ -54,7 +54,7 @@ describe 'Enumerable' do
 
       it "properly performs the block" do
         b = []
-        @array.my_each_with_index { |element, index| b << element + 1}
+        @array.my_each_with_index { |element, index| b << element + 1 }
         expect(b).to eql([2,3,4,5,6])
       end
 
@@ -69,20 +69,82 @@ describe 'Enumerable' do
 
   describe '#my_select' do
     it "returns elements filtered by a boolean" do
-      expect(@array.my_select{|x| x > 2}).to eql([3,4,5])
+      expect(@array.my_select{ |x| x > 2 }).to eql([3,4,5])
     end
 
+    context "when boolean is always true" do
+      before(:all) { @all_true = true }
+      it "returns all elements" do
+        expect(@array.my_select{ |x| @all_true }).to eql(@array)
+      end
+    end
+
+    context "when boolean is always false" do
+      before(:all) { @all_false = false }
+      it "returns an empty array" do
+        expect(@array.my_select { |x| @all_false }).to be_empty
+      end
+    end
   end
 
-  describe '#my_all' do
+  describe '#my_all?' do
+
+    context "when all elements match the boolean" do
+      it "returns true" do
+        expect(@array.my_all? {|x| x < 900 }).to eql(true)
+      end
+    end
+
+    context "when no elements match the boolean" do
+      it "returns false" do
+        expect(@array.my_all? {|x| x > 900 }).to eql(false)
+      end
+    end
 
   end
 
   describe '#my_inject' do
 
+
   end
 
   describe '#my_count' do
+    context "when a value is given and no block" do
+
+      it "returns the number of times that value appears in the object" do
+        expect(@array.my_count(3)).to eql(1)
+      end
+
+      it "returns 0 for a value not found" do
+        expect(@array.my_count("Q")).to eql(0)
+      end
+    end
+
+    context "when a block is given and no value" do
+
+      it "returns the number of elements for which that block returns true" do
+        count = @array.my_count{ |x| x > 3 }
+        expect(count).to eql(2)
+      end
+
+    end
+
+    context "when no block and no value are given" do
+
+      it "returns the size" do
+        expect(@array.my_count).to eql(@array.size)
+      end
+
+    end
+
+    context "when both block and value are given" do
+
+      it "uses the value and ignores the block" do
+        count = @array.my_count(3) { |x| x > 0 }
+        expect(count).to eql(1)
+      end
+
+    end
 
   end
 end
