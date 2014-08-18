445fab2c18a494c98ae9d0abe644dab44c074235
Michael Alexander <alexandermw@gmail.com>
2014-08-17 17:56:55 -0700

    created tictactoe_spec

---
+++ tictactoe_spec.rb
@@ -0,0 +1,113 @@
+require './tictactoe.rb'
+
+describe "GameRunner" do
+
+  describe "#initialize" do
+
+  end
+
+  describe "#get_players" do
+
+  end
+
+  describe "#take_turns" do
+
+  end
+
+  describe "#get_move" do
+
+  end
+
+  describe "#start" do
+
+  end
+
+end
+
+describe "Player" do
+
+  describe "#initialize" do
+
+  end
+
+  describe "#set_name" do
+
+  end
+
+  describe "#set_mark" do
+
+  end
+
+  describe "#mark" do
+
+  end
+
+  describe "#==" do
+
+  end
+
+end
+
+describe "Board" do
+
+  describe "#initialize" do
+
+  end
+
+  describe "#make_move" do
+
+  end
+
+  describe "#position" do
+
+  end
+
+  describe "#value_at" do
+
+  end
+
+  describe "#is_draw?" do
+
+  end
+
+  describe "#three_in_a_row" do
+
+  end
+
+  describe "#winner?" do
+
+  end
+
+  describe "#still_playing?" do
+
+  end
+
+  describe "#draw" do
+
+  end
+
+end
+
+describe "Box" do
+
+  describe "#initialize" do
+
+  end
+
+  describe "#make_mark" do
+
+  end
+
+  describe "#nil?" do
+
+  end
+
+  describe "#full?" do
+
+  end
+
+  describe "#==" do
+
+  end
+
+end
